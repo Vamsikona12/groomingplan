0a852078c525b9c00fbf08277f087628f6dc3bbc
Vamsikona12 <81353340+Vamsikona12@users.noreply.github.com>
2021-07-15 17:09:25 +0530

    Add files via upload

---
+++ Parentclass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritnacedemo
+{
+    class Parentclass
+    {
+        public void Method1()
+        {
+            Console.WriteLine("Method 1 is excuted");
+        }
+        public void Method2()
+        {
+            Console.WriteLine("Method 2 is excuted");
+        }
+    }
+}
